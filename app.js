bde63725794752ec9c1b8db1f664f98e2fc82ff4
reluo <21404683@qq.com>
2018-08-08 11:52:22 +0800

    config new

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// require('dotenv').config()
+const bidnameCloudFuncs = require('./cloud/bidname')
+// const app = require('express')()
+const cors = require('cors')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// app.use('/posts', postRoutes)
+app.get('/getOpenOrderList', bidnameCloudFuncs.getOpenOrderList)
+app.get('/getOrderByAcc', bidnameCloudFuncs.getOrderByAcc)
+app.get('/getOrderBySeller', bidnameCloudFuncs.getOrderListBySeller)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
