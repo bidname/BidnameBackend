aac1723085310297d9e8952d3f7f8c43b8083caf
reluo <21404683@qq.com>
2018-08-09 10:00:28 +0800

    remove /

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // app.use('/posts', postRoutes)
 app.get('/getOpenOrderList', bidnameCloudFuncs.getOpenOrderList)
